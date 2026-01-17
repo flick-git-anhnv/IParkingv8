8b8b9201b3ec97212a7559bb8b6cb50dcb1eddbe
ANHNV_2025 <anhnv09031997@gmail.com>
2026-01-17 08:43:34 +0700

    up

--- ILedv8/Objects/DelayConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ILedv8.Objects
-{
-    public class DelayConfig
-    {
-        public int CheckCountDuration { get; set; } = 2000;
-        public int SendToLedRowDuration { get; set; } = 1000;
-    }
-}
